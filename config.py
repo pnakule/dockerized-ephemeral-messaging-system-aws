7fc78ef9d5532642a31cccfd2db44365e8d2a53a
Pandurang Nakule <pn.devlab@gmail.com>
2026-05-26 14:30:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 DB_CONFIG = {
-    "host": os.getenv("DB_HOST", "xyz"),
-    "user": os.getenv("DB_USER", "xyz"),
-    "password": os.getenv("DB_PASSWORD", "xyz"),
-    "database": os.getenv("DB_NAME", "xyz")
+    "host": os.getenv("DB_HOST", "msql"),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", "Ephemeral@0."),
+    "database": os.getenv("DB_NAME", "ephemeraldb")
 }
