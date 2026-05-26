e4d9a612a0b84ca1eef1149ff0ba835e57dd87bc
Pandurang Nakule <pn.devlab@gmail.com>
2026-05-26 13:33:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,8 @@
-import boto3
-
-# Database configuration
-# values are securely fetched from AWS Systems Manager Parameter Store.
-# This avoids hardcoding credentials and improves security.
-
-ssm = boto3.client("ssm", region_name="us-east-1")
-
-def get_param(name, decrypt=False):
-    return ssm.get_parameter(
-        Name=name,
-        WithDecryption=decrypt
-    )["Parameter"]["Value"]
+import os
 
 DB_CONFIG = {
-    "host": get_param("/ephemeral/db/host"),
-    "user": get_param("/ephemeral/db/user"),
-    "password": get_param("/ephemeral/db/password", decrypt=True),
-    "database": get_param("/ephemeral/db/name"),
-    "port": 3306,
+    "host": os.getenv("DB_HOST", "xyz"),
+    "user": os.getenv("DB_USER", "xyz"),
+    "password": os.getenv("DB_PASSWORD", "xyz"),
+    "database": os.getenv("DB_NAME", "xyz")
 }
